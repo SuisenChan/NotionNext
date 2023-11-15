0afc788fbc60ddb41caa8a655ea352e15ac79298
SuisenChan <119867806+SuisenChan@users.noreply.github.com>
2023-11-15 17:57:06 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -20,7 +20,7 @@ const BLOG = {
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '是水水呀', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '一个专属于水水同学的储物间，欢迎来参观呀~', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://shuishui666.cn', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '学习笔记, 日常记录,好网站分享', // 网站关键词 英文逗号隔开
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '水水的日常记录', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || '2210323219@qq.com', // 邮箱地址 例如mail@tangly1024.com
